8bd5007848bcc57e0b661273cb0765c125126a29
Phillip Nguyen <phucnguyen290198@gmail.com>
2022-07-09 23:58:16 +0700

    linked_list

--- chapter 2/linked_list.py
+++ chapter 2/linked_list.py
@@ -75,6 +75,117 @@ class LinkedList:
 
         return False
 
+    def removeDup(self):
+        node = self.head
+        if node is None or node.next is None:
+            return
+
+        # else
+        # hash_map
+        hash_map = {}
+        prev_node = self.head
+        while node:
+            if node.data in hash_map:
+                prev_node.next = node.next
+
+            elif node.data not in hash_map:
+                hash_map[node.data] = 1
+                prev_node = node
+
+            node = node.next
+
+    def find_element(self, data):
+        node = self.head
+        if node is None or node.next is None:
+            return node.data
+
+        index = 0
+        while node:
+            if node.data == data:
+                return index
+
+            else:
+                node = node.next
+                index += 1
+
+        return False
+
+    def kth_to_last(self, k):
+        # length - k
+        length = self.get_length()
+        print(length)
+        true_length = length - k
+        node = self.head
+        # assuming return last el if k >= length
+        i = 0
+        while node and i < true_length:
+            node = node.next
+            i += 1
+
+        return node.data
+
+
+    def delete_mid(self, data):
+        node = self.head
+        # if data at head -> not removing
+        if node.data == data:
+            return
+
+        # if data at end -> not removing
+        while node:
+            if node.data == data and node.next is None:
+                return
+            node = node.next
+
+        # else, remove data in the middle
+        self.remove(data)
+
+    # (2 -> 6 -> 3) + (3 -> 5 -> 4) = 362 + 453 = 815 -> (5 -> 1 -> 8)
+    def sum_list(self, list1, list2):
+        # keep track of each list
+        cur_list1 = list1.head # head of a linked list is a node
+        cur_list2 = list2.head
+        length = list1.get_length() - 1
+        sum = 0
+        i = 0
+
+        while i <= length and cur_list1 and cur_list2:
+            sum += (pow(10, i) * cur_list1.data) + (pow(10, i) * cur_list2.data)
+            i += 1
+            cur_list1 = cur_list1.next
+            cur_list2 = cur_list2.next
+
+        node = Node(sum % 10)
+        curList = LinkedList()
+        node.next = curList.head
+        curList.head = node
+        curNode = curList.head
+        sum = int(sum / 10)
+        while sum > 0:
+            while curNode.next:
+                curNode = curNode.next
+            node = Node(sum % 10)
+            node.next = curNode.next
+            curNode.next = node
+            sum = int(sum / 10)
+            curNode = curList.head
+
+        return curList
+
+
+    def partition(self, data):
+        return
+
+    def get_length(self):
+        length = 0
+        node = self.head
+        while node:
+            length += 1
+            node = node.next
+
+        return length
+
+
     def traverse(self):
         # we need to traverse through the singly linked list
         node = self.head
@@ -86,16 +197,32 @@ class LinkedList:
         print(None)
 
 
+
+
+
 linked_list = LinkedList()
 linked_list.add(6)
 linked_list.insert(4, 0)
 linked_list.insert(10, 1)
+linked_list.insert(10, 1)
+linked_list.addEnd(1248)
 linked_list.addEnd(1248)
 linked_list.traverse()
-# linked_list.remove(6)
-linked_list.remove(4)
-
+linked_list.removeDup()
+linked_list.traverse()
+# print(linked_list.kth_to_last(4))
+linked_list.delete_mid(1248)
+linked_list.delete_mid(4)
+# linked_list.delete_mid(10)
 linked_list.traverse()
 
-
-
+list = LinkedList()
+list1 = LinkedList()
+list1.add(3)
+list1.add(9)
+list1.add(3)
+list2 = LinkedList()
+list2.add(1)
+list2.add(2)
+list2.add(3)
+list.sum_list(list1, list2).traverse()
